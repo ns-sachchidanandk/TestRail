b0f59bc0c197f3f284ff8f80fdd6477188642cb1
Sachchidanand Kumar <sachchidanandk@netskope.com>
2025-01-30 14:02:26 +0530

    add update_stack_label_field.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 import pandas
+
 from testrail import *
 
 client = APIClient(base_url="https://netskope.testrail.io/")
-client.user = "" # your testrail email address goes here
-client.password = "" # your testrail password goes here
+client.user = "sachchidanandk@netskope.com" # your testrail email address goes here
+client.password = "2Ur!$%B7A$@8GivK&" # your testrail password goes here
 
 PROJECT_ID = 34
 SUITE_ID = 921
@@ -17,23 +18,27 @@ data = {
     "custom_ui_case": 1,
 }
 
-def split_list(lst, chunk_size):
-    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+breakpoint()
+response = client.send_get("get_case/1498430")
+print(json.dump(response, indent=4))
 
-for row in df.itertuples(index=True):
-    if row.Suite == "SWG/N4W" and f"{row._14}".strip().lower() == "no":
-        ids.append(int(row.ID.lstrip("C")))
-
-for index, lst in enumerate(split_list(ids, 10)):
-    if index < 445:
-        continue
-
-    data["case_ids"] = lst
-
-    response = client.send_post(f"update_cases/{SUITE_ID}", data=data)
-
-    with open(f"data_{index}.json", "w") as fp:
-        fp.write(json.dumps(data, indent=4))
-
-    with open(f"update_cases_{index}.json", "w") as fp:
-        fp.write(json.dumps(response, indent=4))
+# def split_list(lst, chunk_size):
+#     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
+#
+# for row in df.itertuples(index=True):
+#     if row.Suite == "SWG/N4W" and f"{row._14}".strip().lower() == "no":
+#         ids.append(int(row.ID.lstrip("C")))
+#
+# for index, lst in enumerate(split_list(ids, 10)):
+#     if index < 445:
+#         continue
+#
+#     data["case_ids"] = lst
+#
+#     response = client.send_post(f"update_cases/{SUITE_ID}", data=data)
+#
+#     with open(f"data_{index}.json", "w") as fp:
+#         fp.write(json.dumps(data, indent=4))
+#
+#     with open(f"update_cases_{index}.json", "w") as fp:
+#         fp.write(json.dumps(response, indent=4))
---
+++ update_stack_label_field.py
@@ -0,0 +1,40 @@
+import json
+
+from typing import List
+from testrail import *
+
+
+
+def parse_test_case_ids(file_path: str = "./data/uniq_test_case_ids.txt") -> List:
+    test_case_ids: List
+    with open(file_path, "r") as fp:
+        test_case_ids = fp.readlines()
+
+    return test_case_ids
+
+def fetch_test_case_data(client: APIClient, test_case_id: int):
+    response = client.send_get(f"get_case/{test_case_id}")
+    return response
+
+def create_test_case_update_data(test_case_data: dict):
+    custom_stack_label = test_case_data.get("custom_stack_label", [])
+    if 4 not in custom_stack_label:
+        custom_stack_label.append(4)
+
+    return {
+        "custom_stack_label": custom_stack_label
+    }
+
+if __name__ == "__main__":
+    client = APIClient(base_url="https://netskope.testrail.io/")
+    client.user = "sachchidanandk@netskope.com"
+    client.password = "2Ur!$%B7A$@8GivK&"
+
+    test_case_ids = parse_test_case_ids()
+    for id in test_case_ids:
+        print(f"Updating test case with id: {id}")
+        id = int(id.strip())
+        test_case_data = fetch_test_case_data(client, id)
+        data = create_test_case_update_data(test_case_data)
+        client.send_post(f"update_case/{id}", data=data)
+        updated_test_case_data = fetch_test_case_data(client, id)
